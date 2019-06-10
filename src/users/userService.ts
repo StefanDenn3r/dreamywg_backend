f8e46432cc8fc3c834d9f81d5e9fcea615e8b88c
stefan <stefand96@gmx.de>
2019-06-10 22:58:49 +0200

    implementation of registration email, waiting for frontend

---
+++ src/users/userService.ts
@@ -0,0 +1,38 @@
+import {default as User, IUserModel} from './user'
+import Token, {ITokenModel} from "../tokens/token";
+import {Request, Response} from "express";
+import * as crypto from 'crypto'
+import * as nodemailer from 'nodemailer'
+import * as config from 'config'
+
+
+export let sendVerificationMail = async (req: Request, res: Response, userId) => {
+    //const userId = await req.params.id;
+    let user: IUserModel = await User.findById(userId);
+    let token: ITokenModel = new Token({_userId: user._id, token: crypto.randomBytes(16).toString('hex')});
+
+    try {
+        await token.save();
+    } catch (err) {
+        return res.status(500).send({msg: err.message});
+    }
+
+    // Send email
+    const nodemailerOptions = config.get('nodemailer');
+    const transporter = nodemailer.createTransport(nodemailerOptions);
+    const mailOptions = {
+        from: nodemailerOptions.auth.user,
+        to: user.email,
+        subject: 'Account Verification Token',
+        text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/confirmation\/' + token.token + '.\n'
+    };
+    try {
+        transporter.sendMail(mailOptions)
+    } catch (err) {
+        return res.status(500).send({msg: err.message});
+    }
+
+    return res.status(200).send();
+};
+
+
