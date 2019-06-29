ec26668f8ac5b18b0a2ff399db597d666a124a10
Patipon Riebpradit <patipon74@gmail.com>
2019-06-29 19:23:08 +0200

    completed flatshareofferer API (tested)

--- src/preferences/flatOfferer.ts
+++ src/preferences/flatOfferer.ts
@@ -1,34 +1,81 @@
-import {Document, Model, model, Schema} from "mongoose";
+import { Document, Model, model, Schema } from "mongoose";
 
+interface Roommate {
+  nationality: string;
+  age: number;
+  occupation: string;
+  description: string;
+}
+
+enum GENDER {
+  MALE_ONLY = "MALE_ONLY",
+  FEMALE_ONLY = "FEMALE_ONLY",
+  BOTH = "BOTH"
+}
 
 interface IOfferer {
-    location?: string;
-    rentType?: string;
-    flatType?: string;
-    nearbyStation?: [string];
-    nearbyStore?: [string];
-    miscellaneous?: [string];
-    flooring?: [string];
-    photo?: boolean;
+  location?: string;
+  rentType?: string;
+  flatType?: string;
+  nearbyStation?: [string];
+  nearbyStore?: [string];
+  miscellaneous?: [string];
+  flooring?: [string];
+  photo?: boolean;
+  roommate?: [Roommate];
+  gender?: GENDER;
+  minAge?: number;
+  maxAge?: number;
+  occupation?: string;
+  flatshareExperience: string;
+  practiceOfAbstaining: string;
+  cleanliness: string;
+  cleaningSchedule: string;
+  activities: [string];
+  smokersAllowed: boolean;
+  petsAllowed: boolean;
 }
 
-export interface IOffererModel extends IOfferer, Document {
-}
+export interface IOffererModel extends IOfferer, Document {}
 
-export var FlatOffererSchema: Schema = new Schema({
-    location: Map,
+export const FlatOffererSchema: Schema = new Schema(
+  {
+    location: String,
     rentType: String,
     flatType: String,
     nearbyStation: [String],
     nearbyStore: [String],
-    miscellaneous:[String],
+    miscellaneous: [String],
     flooring: [String],
-    photo: { data: Buffer, contentType: String }
-}, {versionKey: false});
-
+    photo: { data: Buffer, contentType: String },
+    roommate: [
+      {
+        nationality: String,
+        age: Number,
+        occupation: String,
+        description: String
+      }
+    ],
+    gender: { type: String, enum: this.GENDER, default: GENDER.BOTH },
+    minAge: Number,
+    maxAge: Number,
+    occupation: String,
+    flatshareExperience: String,
+    practiceOfAbstaining: String,
+    cleanliness: String,
+    cleaningSchedule: String,
+    activities: [String],
+    smokersAllowed: Boolean,
+    petsAllowed: Boolean
+  },
+  { versionKey: false }
+);
 
 // transformer : should be separated in different file if big enough
-FlatOffererSchema.set('toJSON')
+FlatOffererSchema.set("toJSON");
 
-const FlatOfferer: Model<IOffererModel> = model("FlatOfferer", FlatOffererSchema);
-export default FlatOfferer
+const FlatOfferer: Model<IOffererModel> = model(
+  "FlatOfferer",
+  FlatOffererSchema
+);
+export default FlatOfferer;
--- src/server.ts
+++ src/server.ts
@@ -1,77 +1,98 @@
-import * as bodyParser from 'body-parser'
-import * as compression from 'compression'
-import * as helmet from 'helmet';
-import * as cors from 'cors';
-import * as errorHandler from './utils/errorHandler'
-import * as express from 'express'
-import * as config from 'config';
-import * as morgan from 'morgan'
-import * as mongoose from 'mongoose'
-import {APILogger, WinstonStream} from './utils/logger'
+import * as bodyParser from "body-parser";
+import * as compression from "compression";
+import * as helmet from "helmet";
+import * as cors from "cors";
+import * as errorHandler from "./utils/errorHandler";
+import * as express from "express";
+import * as config from "config";
+import * as morgan from "morgan";
+import * as mongoose from "mongoose";
+import { APILogger, WinstonStream } from "./utils/logger";
 
-
-import {UserRoute} from './users/userAPI'
+import { FlatOffererRoute } from "./preferences/flatOffererAPI";
+import { UserRoute } from "./users/userAPI";
 
 class Server {
-    public app: express.Application;
-    public userRoutes: UserRoute = new UserRoute();
-    public env: string = process.env.NODE_ENV || 'development';
-    public port: number | string;
-    private mongoUrl: string = config.get('mongo.URI');
+  public app: express.Application;
+  public userRoutes: UserRoute = new UserRoute();
+  public flatOffererRoutes: FlatOffererRoute = new FlatOffererRoute();
+  public env: string = process.env.NODE_ENV || "development";
+  public port: number | string;
+  private mongoUrl: string = config.get("mongo.URI");
 
-    constructor(port: number | string = 5003) {
-        this.app = express();
-        this.app.set('port', port);
-        this.app.set('env', this.env);
-        this.app.use(bodyParser.json());
-        this.app.use(bodyParser.urlencoded({extended: true}));
-        this.applyLogger();
-        this.applyRoutes();
-        this.applyMiddleWare();
-        this.start()
+  constructor(port: number | string = 5003) {
+    this.app = express();
+    this.app.set("port", port);
+    this.app.set("env", this.env);
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.applyLogger();
+    this.applyRoutes();
+    this.applyMiddleWare();
+    this.start();
+  }
+
+  private async start() {
+    await this.mongoSetup();
+    this.app.listen(this.app.get("port"), () => {
+      APILogger.logger.info(
+        `Server [${config.get(
+          "name"
+        )}] is running at http://localhost:${this.app.get(
+          "port"
+        )} in ${this.app.get("env")} Mode`
+      );
+      APILogger.logger.info("Press CTRL-C to stop");
+    });
+  }
+
+  private applyLogger(): void {
+    this.app.use(
+      morgan(config.get("morganFormat"), { stream: new WinstonStream() })
+    );
+  }
+
+  private applyMiddleWare(): void {
+    this.app.use(helmet());
+    this.app.use(cors());
+    this.app.use(compression());
+    this.app.use(errorHandler.logging);
+    this.app.use(errorHandler.clientErrorHandler);
+    this.app.use(errorHandler.errorHandler);
+    APILogger.logger.info(
+      "Applied middleware: [HELMET][CORS][COMPRESSION][LOGGING][ERROR HANDLER]"
+    );
+  }
+
+  private applyRoutes(): void {
+    this.app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      );
+      next();
+    });
+
+    UserRoute.routes(this.app);
+    FlatOffererRoute.routes(this.app);
+    APILogger.logger.info(
+      "Applied Routes: [USER][AUTHENTICATION][BUSINESS LOGIC]"
+    );
+  }
+
+  private async mongoSetup() {
+    try {
+      await mongoose.connect(this.mongoUrl, config.get("mongo.config"));
+      APILogger.logger.info(
+        `Connection to MongoDB at ${this.mongoUrl} established`
+      );
+    } catch (err) {
+      APILogger.logger.error(
+        `Connection to MONGO DB failed : ${err} - Shutting down Server`
+      );
     }
-
-    private async start() {
-        await this.mongoSetup();
-        this.app.listen(this.app.get('port'), () => {
-            APILogger.logger.info(`Server [${config.get('name')}] is running at http://localhost:${this.app.get('port')} in ${this.app.get('env')} Mode`);
-            APILogger.logger.info('Press CTRL-C to stop')
-        })
-    }
-
-    private applyLogger(): void {
-        this.app.use(morgan(config.get('morganFormat'), {stream: new WinstonStream()}))
-    }
-
-    private applyMiddleWare(): void {
-        this.app.use(helmet());
-        this.app.use(cors());
-        this.app.use(compression());
-        this.app.use(errorHandler.logging);
-        this.app.use(errorHandler.clientErrorHandler);
-        this.app.use(errorHandler.errorHandler);
-        APILogger.logger.info('Applied middleware: [HELMET][CORS][COMPRESSION][LOGGING][ERROR HANDLER]')
-    }
-
-    private applyRoutes(): void {
-        this.app.use(function(req, res, next) {
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-            next();
-        });
-
-        UserRoute.routes(this.app);
-        APILogger.logger.info('Applied Routes: [USER][AUTHENTICATION][BUSINESS LOGIC]')
-    }
-
-    private async mongoSetup() {
-        try {
-            await mongoose.connect(this.mongoUrl, config.get('mongo.config'));
-            APILogger.logger.info(`Connection to MongoDB at ${this.mongoUrl} established`)
-        } catch (err) {
-            APILogger.logger.error(`Connection to MONGO DB failed : ${err} - Shutting down Server`)
-        }
-    }
+  }
 }
 
-export default Server
+export default Server;
