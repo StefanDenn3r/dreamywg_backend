bf44d580e608464dada4f141f6aeacf105847fb5
Gabrielle Wicesawati Poerwawinata <gabybongbong@gmail.com>
2019-06-30 22:03:07 +0200

    chat modify

--- src/chat/chat-message.ts
+++ src/chat/chat-message.ts
@@ -1,8 +1,8 @@
 import { Message, User } from './';
 
 export class ChatMessage extends Message{
-    constructor(from: User, content: string) {
-        super(from, content);
+    constructor(from: User, content: string, timestamp: Date) {
+        super(from, content, timestamp);
     }
 }
 
--- src/chat/message.ts
+++ src/chat/message.ts
@@ -1,5 +1,6 @@
+import {Runtime} from "inspector";
 import {User} from './user';
 
 export class Message {
-    constructor(private from: User, private content: string, private timestamp: time) {}
+    constructor(private from: User, private content: string, private timestamp: Date) {}
 }
