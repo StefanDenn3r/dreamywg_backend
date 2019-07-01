d8f0fa7600744498f1cfa372963f06c68c872d2d
Bukhori Aqid <aqid@Bukhoris-Air.fritz.box>
2019-07-01 10:59:39 +0200

    [update] integrate token & redirect with saved parameter

--- src/users/tokens/tokenLinkedin.ts
+++ src/users/tokens/tokenLinkedin.ts
@@ -1,20 +1,4 @@
-import {Document, Model, model, Schema} from "mongoose";
-
-
 export interface ITokenLinkedin {
     access_token?: string;
     expires_in?: Date;
 }
-
-export interface ITokenModelLinkedin extends ITokenLinkedin, Document {
-
-}
-
-export var tokenSchema: Schema = new Schema({
-    access_token: {type: String, required: true},
-    expires_in: {type: Date, required: true, default: Date.now, expires: 43200}
-}, {versionKey: false});
-
-
-const TokenLinkedin: Model<ITokenModelLinkedin> = model<ITokenModelLinkedin>("TokenLinkedin", tokenSchema);
-export default TokenLinkedin
