d96412c90e40ff538ebda08c104179019ff1f895
Bukhori Aqid <aqid@Bukhoris-Air.fritz.box>
2019-07-08 00:17:44 +0200

    [add] init schedule api

---
+++ src/schedules/scheduleAPI.ts
@@ -0,0 +1,10 @@
+import * as scheduleController from './scheduleController'
+
+export class ScheduleRoute {
+    public static routes(app): void {
+        app.route('/schedules').get(scheduleController.getSchedules);
+        app.route('/schedules/:id').get(scheduleController.getSchedule);
+        app.route('/schedules/:id/timeslot').post(scheduleController.createTimeslot);
+        app.route('/schedules/:id/timeslot').put(scheduleController.updateTimeslot);
+    }
+}
