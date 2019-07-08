b038e715ebdbce53d5b17284ea354bd062696e56
Aqid <aqid.bukhori@flixbus.com>
2019-07-09 00:08:16 +0200

    [update] halfway into schedule crud

--- src/schedules/scheduleAPI.ts
+++ src/schedules/scheduleAPI.ts
@@ -4,6 +4,7 @@ export class ScheduleRoute {
     public static routes(app): void {
         app.route('/schedules').get(scheduleController.getSchedules);
         app.route('/schedules/:id').get(scheduleController.getSchedule);
+        app.route('/schedules').post(scheduleController.createSchedule);
         app.route('/schedules/:id/timeslot').post(scheduleController.createTimeslot);
         app.route('/schedules/:id/timeslot').put(scheduleController.updateTimeslot);
     }
