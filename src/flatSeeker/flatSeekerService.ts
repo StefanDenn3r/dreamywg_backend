8ca8f3ac7df406c0da91b9fbdf7c9a5e7202ab1b
stefan <stefand96@gmx.de>
2019-07-09 15:06:45 +0200

    [c] continued

--- src/flatSeeker/flatSeekerService.ts
+++ src/flatSeeker/flatSeekerService.ts
@@ -1,5 +1,6 @@
 import {IFlatSeekerModel} from "./flatSeeker";
 import {IFlatModel} from "../flats/flat";
+import {rentType} from "../utils/selectionEnums";
 
 export let match = (flatSeeker: IFlatSeekerModel, flats: IFlatModel[]) => {
     const personalInformation = flatSeeker.personalInformation;
@@ -7,8 +8,18 @@ export let match = (flatSeeker: IFlatSeekerModel, flats: IFlatModel[]) => {
     flats
         .filter(flat => preferences.flat.regions.includes(flat.region))
         .filter(flat => preferences.flat.flatshareType === flat.flatshareType)
-        .filter(flat => inBetween(preferences.flat.room.size, flat.rooms[0]. )
+        .filter(flat => inBetween(preferences.flat.room.size, flat.rooms[0].roomSize))
+        .filter(flat => inBetween(preferences.flat.room.rent, flat.rooms[0].rent))
+        .filter(flat => preferences.flat.room.rentType === flat.rooms[0].rentType)
+        .filter(flat => {
+            if(preferences.flat.room.rentType === rentType.UNLIMITED)
+                return inBetweenDate(preferences.flat.room.dateAvailable, flat.rooms[0].dateAvailable)
+        })
+        .filter(flat => preferences.flat.room.furnished === flat.rooms[0].furnished)
 }
 
 
 let inBetween = (range, value) => range.from <= value && value <= range.to;
+let inBetweenDate = (range, value) => range[0] <= value && value <= range[1]; // check if this works with date as well
+
+let dateAvailable = (range, value) => range.from <= value && value <= range.to;
