61f96d9f52bd95ab0dc8bb66c3a88098aa143bb7
Gabrielle Wicesawati Poerwawinata <gabybongbong@gmail.com>
2019-07-12 00:31:04 +0200

    update

--- src/chat/messageUnit.ts
+++ src/chat/messageUnit.ts
@@ -1,7 +1,6 @@
 import {Document, Model, model, Schema} from "mongoose";
 
 interface MessageUnit{
-    _messageId?: Schema.Types.ObjectId;
     user1?: string;
     user2?: string;
     messages?: [{
@@ -16,7 +15,6 @@ export interface MessageUnitModel extends MessageUnit, Document {
 }
 
 export var messageUnitSchema: Schema = new Schema({
-    _messageId: {type: Schema.Types.ObjectId, required: true, ref: 'MessageUnit'},
     user1: {type: String, required: true},
     user2: {type: String, required: true},
     messages : [{
