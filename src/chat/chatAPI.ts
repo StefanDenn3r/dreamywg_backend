7e0667b129cd3696154add95193726a988a17a7d
stefan <stefand96@gmx.de>
2019-07-16 01:12:44 +0200

    [c] code cleanup, refactoring, bug fixes

--- src/chat/chatAPI.ts
+++ src/chat/chatAPI.ts
@@ -3,11 +3,10 @@ import * as chatController from './chatController'
 
 export class ChatRoute {
     public static routes(app): void {
-        console.log("chat route called")
         app.route('/chat').get(chatController.retrieveChatList);
         app.route('/chatunit').get(chatController.retrieveChatUnit);
         app.route('/chatunit').delete(chatController.deleteChat);
-        app.route('/initchat').post(chatController.initChatwithAllUsers);
+        app.route('/initchat').post(chatController.initChatWithAllUsers);
         app.route('/chat').delete(chatController.removeAllChat);
     }
 }
