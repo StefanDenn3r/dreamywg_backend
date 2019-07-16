2b3de82706cc7e27fe18edc5bdfab21a7f9493c4
stefan <stefand96@gmx.de>
2019-07-16 13:29:04 +0200

    don't destroy!

--- src/chat/chatAPI.ts
+++ src/chat/chatAPI.ts
@@ -4,7 +4,7 @@ import * as chatController from './chatController'
 export class ChatRoute {
     public static routes(app): void {
         app.route('/chat').get(chatController.retrieveChatList);
-        app.route('/chatunit').get(chatController.retrieveChatUnit);
+        // app.route('/chatunit').get(chatController.retrieveChatUnit);
         app.route('/chatunit').delete(chatController.deleteChat);
         app.route('/initchat').post(chatController.initChatWithAllUsers);
         app.route('/chat').delete(chatController.removeAllChat);
