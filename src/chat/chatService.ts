d10ccef9b8c0c0be00cc08cfeb691a9b1eafba61
stefan <stefand96@gmx.de>
2019-07-19 19:41:50 +0200

    chat todo: Schedule

--- src/chat/chatService.ts
+++ src/chat/chatService.ts
@@ -1,59 +1,107 @@
 import {UserService} from "../users/userService";
-import MessageUnit from "./messageUnit";
 import {Logger} from "../utils/logger";
+import {Chat} from "./chat";
+import {FlatOffererService} from "../flatOfferer/flatOffererService";
+import {createNewChat} from "../../dist/chat/chatController";
 
-export const retrieveChatlist = async (token) => {
-    const user = await UserService.getUserByToken(token);
-    const messageList = await MessageUnit.find({$or: [{'user1.id': user._id.toString()}, {'user2.id': user._id.toString()}]}).sort({"messages.timestamp": 1});
+export class ChatService {
+    static retrieveChatList = async (token) => {
+        const user = await UserService.getUserByToken(token);
 
-    if (!messageList)
-        return null;
+        if (!user)
+            return null;
 
-    const chats = {};
-    messageList.forEach(element => {
-        const receiverId = (element.user1.id === user._id.toString()) ? element.user2.id : element.user1.id;
-        chats[receiverId] = element;
-    });
-    return chats;
-};
+        try {
+            const messageList = await Chat.find({$or: [{'user1.id': user._id.toString()}, {'user2.id': user._id.toString()}]}).sort({"messages.timestamp": 1});
+            const chats = {};
+            messageList.forEach(element => {
+                const receiverId = (element.user1.id === user._id.toString()) ? element.user2.id : element.user1.id;
+                chats[receiverId] = element;
+            });
+            return chats;
+        } catch (e) {
+            Logger.logger.error(e);
+            return null
+        }
+    };
 
-export let getChatUnit = async (user1, user2) => {
-    try {
-        return await MessageUnit.findOne({$or: [{$and: [{'user1.id': user1.id}, {'user2.id': user2.id}]}, {$and: [{'user1.id': user2.id}, {'user2.id': user1.id}]}]})
-    } catch (e) {
-        console.log(e);
-        return null;
-    }
-}
+    static getChat = async (user1, user2) => {
+        try {
+            return await Chat.findOne({$or: [{$and: [{'user1.id': user1.id}, {'user2.id': user2.id}]}, {$and: [{'user1.id': user2.id}, {'user2.id': user1.id}]}]})
+        } catch (e) {
+            Logger.logger.error(e);
+            return null
+        }
+    };
 
-export let updateChatUnit = async (user1, user2, senderId, content, timestamp: Date) => {
-    try {
-        await MessageUnit.update({$or: [{$and: [{'user1.id': user1.id}, {'user2.id': user2.id}]}, {$and: [{'user1.id': user2.id}, {'user2.id': user1.id}]}]}, {
-            $push: {
-                messages: {
-                    senderId: senderId,
-                    content: content,
-                    timestamp: timestamp
+    static updateChatUnit = async (user1, user2, senderId, content, timestamp: Date) => {
+        try {
+            await Chat.update({$or: [{$and: [{'user1.id': user1.id}, {'user2.id': user2.id}]}, {$and: [{'user1.id': user2.id}, {'user2.id': user1.id}]}]}, {
+                $push: {
+                    messages: {
+                        senderId: senderId,
+                        content: content,
+                        timestamp: timestamp
+                    }
                 }
-            }
+            });
+        } catch (e) {
+            Logger.logger.error(e);
+            return null
+        }
+    };
+
+    static async deleteAllChats() {
+        try {
+            return await Chat.deleteMany({});
+        } catch (e) {
+            Logger.logger.error(e);
+            return null
+        }
+    }
+
+
+    static createChatByTokenAndFlatId = async (token, flatId) => {
+        let currentUser = await UserService.getUserByToken(token);
+        const flatOfferer = await FlatOffererService.getFlatOffererByFlatId(flatId);
+
+        if (!currentUser || !flatOfferer)
+            return null;
+        const currentMessageUser = ChatService.convertToMessageUser(currentUser);
+        const targetMessageUser = ChatService.convertToMessageUser(flatOfferer.user);
+
+        const chat = await ChatService.getChat(currentMessageUser, targetMessageUser);
+
+        if (!chat)
+            await createNewChat(currentMessageUser, targetMessageUser);
+
+        const chats = await ChatService.retrieveChatList(token);
+
+        return {
+            chats: chats,
+            userId: targetMessageUser.id
+        }
+    };
+
+    static convertToMessageUser = (user) => {
+        return {
+            id: user.id,
+            fullName: user.fullName()
+        }
+    };
+
+    private static createChat = async (user1, user2) => {
+        const Chat = new Chat({
+            user1: user1,
+            user2: user2,
+            messages: []
         });
-    } catch (e) {
-        console.log(e);
-        return null;
-    }
-};
 
-export let createNewChat = async (user1, user2) => {
-    const newMessageUnit = new MessageUnit({
-        user1: user1,
-        user2: user2,
-        messages: []
-    });
-
-    try {
-        await newMessageUnit.save();
-    } catch (err) {
-        Logger.logger.error(`[POST] [/users] something went wrong when saving a new message # ${err.message}`);
-    }
-};
-
+        try {
+            await Chat.save();
+        } catch (e) {
+            Logger.logger.error(e);
+            return null
+        }
+    };
+}
