ab3928b2c363d8c2f18e79efe1f38144f16092ee
stefan <stefand96@gmx.de>
2019-07-19 19:41:50 +0200

    chat todo: Schedule

--- src/chat/chatController.ts
+++ src/chat/chatController.ts
@@ -1,85 +1,40 @@
 import {NextFunction, Request, Response} from "express";
-import {User} from '../users/user'
-import {Logger} from "../utils/logger";
-import MessageUnit from "./messageUnit";
-import {createNewChat, getChatUnit, retrieveChatlist} from "./chatService";
-import {UserService} from "../users/userService";
-import {FlatOffererService} from "../flatOfferer/flatOffererService";
+import {ChatService} from "./chatService";
+
+export class ChatController {
+
+    static createChat = async (req: Request, res: Response, next: NextFunction) => {
+        const token = req.header('Authorization');
+        const flatId = req.params.id;
+
+        const chats = ChatService.createChatByTokenAndFlatId(token, flatId);
+
+        if (!chats)
+            return res.status(400).send();
+        else
+            return res.json(chats)
+
+    };
+
+    static retrieveChatList = async (req: Request, res: Response, next: NextFunction) => {
+        const token = req.header('Authorization');
 
 
-export let createChat = async (req: Request, res: Response, next: NextFunction) => {
-    const flatId = req.params.id;
-    const token = req.header('Authorization');
+        const chats = await ChatService.retrieveChatList(token);
+        if (!chats)
+            return res.status(400).send();
+        else
+            return res.json(chats)
 
-    try {
-        const currentUser = convertToMessageUser(await UserService.getUserByToken(token));
-        const flatOfferer = await FlatOffererService.getFlatOffererByFlatId(flatId);
-        const targetUser = convertToMessageUser(flatOfferer.user);
+    };
 
-        const chatUnit = await getChatUnit(currentUser, targetUser);
+    static removeAllChat = async (req: Request, res: Response, next: NextFunction) => {
+        const chats = await ChatService.deleteAllChats();
 
-        if (!chatUnit)
-            await createNewChat(currentUser, targetUser);
+        if (!chats)
+            return res.status(400).send();
+        else
+            return res.json(chats)
 
-        const chats = await retrieveChatlist(token);
-
-        return res.status(200).send({
-                chats: chats,
-                userId: targetUser.id
-            }
-        );
-    } catch (err) {
-        return res.status(400).send(err)
-    }
-};
-
-
-export let retrieveChatList = async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.header('Authorization');
-
-    try {
-        const chats = await retrieveChatlist(token);
-        return res.json(chats)
-    } catch (err) {
-        return res.status(400).send(err)
-    }
-};
-
-export let deleteChat = async (req: Request, res: Response, next: NextFunction) => {
-    const id = req.query._id;
-
-    Logger.logger.warn(`[DELETE] [/users] ${id}`);
-
-    await MessageUnit.findByIdAndDelete(id);
-
-    return res.status(204).send()
-
-};
-
-export let initChatWithAllUsers = async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.header('Authorization');
-    const currentuser = await UserService.getUserByToken(token);
-    const users = await User.find();
-    users.forEach(async user => {
-        if (user._id.toString() !== currentuser._id.toString()) {
-            await createNewChat(convertToMessageUser(currentuser), convertToMessageUser(user));
-        }
-    });
-    return res.status(200).send();
-};
-
-const convertToMessageUser = (user) => {
-    return {
-        id: user.id,
-        fullName: user.fullName()
-    }
-};
-
-export let removeAllChat = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        await MessageUnit.remove({});
-        return res.status(204).send()
-    } catch (e) {
-        return res.status(400).send(e)
-    }
-};
+    };
+}
