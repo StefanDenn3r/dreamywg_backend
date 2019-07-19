328543c39bf4d658dc852b2d6320a43f1172fc52
stefan <stefand96@gmx.de>
2019-07-19 19:14:09 +0200

    Flatseeker refactoring todo: Chat, Schedule

--- src/flatSeeker/flatSeekerAPI.ts
+++ src/flatSeeker/flatSeekerAPI.ts
@@ -1,12 +1,12 @@
-import * as flatSeekerController from "./flatSeekerController"
+import {FlatSeekerController} from "./flatSeekerController";
 
 export class FlatSeekerRoute {
     public static routes(app): void {
-        app.route("/flatseekers/loadSearchProperties").get(flatSeekerController.loadSearchProperties);
-        app.route("/flatseekers/search").post(flatSeekerController.searchFlats);
-        app.route("/flatseekers/:id").get(flatSeekerController.getFlatSeeker);
-        app.route("/flatseekers").get(flatSeekerController.getFlatSeekers);
-        app.route("/flatseekers").post(flatSeekerController.addFlatSeeker)
-        app.route("/flatseekers").delete(flatSeekerController.deleteAllFlatSeekers)
+        app.route("/flatseekers/loadSearchProperties").get(FlatSeekerController.loadSearchProperties);
+        app.route("/flatseekers/search").post(FlatSeekerController.searchFlats);
+        app.route("/flatseekers/:id").get(FlatSeekerController.getFlatSeeker);
+        app.route("/flatseekers").get(FlatSeekerController.getFlatSeekers);
+        app.route("/flatseekers").post(FlatSeekerController.createFlatSeeker);
+        app.route("/flatseekers").delete(FlatSeekerController.deleteAllFlatSeekers)
     }
 }
