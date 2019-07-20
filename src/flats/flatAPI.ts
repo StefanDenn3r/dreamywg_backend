a126b1d5b119ad0b687c0fc39f6b5f5894bf5266
Aqid <aqid.bukhori@flixbus.com>
2019-07-20 14:11:24 +0200

    [add] my flat api

--- src/flats/flatAPI.ts
+++ src/flats/flatAPI.ts
@@ -5,6 +5,7 @@ export class FlatRoute {
         app.route('/flats/generate').get(flatController.generateFlats);
         app.route('/flats').get(flatController.getFlats);
         app.route('/flats').post(flatController.addFlat);
+        app.route('/flats/my-flat').get(flatController.getOffererFlat);
         app.route('/flats/:id').get(flatController.getFlat);
         app.route("/flats/:id").delete(flatController.deleteFlat);
         app.route("/flats").delete(flatController.deleteAllFlats)
